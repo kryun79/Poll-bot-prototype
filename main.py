f5b250d82693addbad66b7b3415b58781e451cee
Casey Y <>
2021-05-30 20:35:00 +0000

    Repeats text working on getting reactions built in.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import random
-
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -11,13 +11,24 @@ GUILD = os.environ['Discord_Guild']
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='repeat', help = 'will eventualy repeat the user\'s text')
+Emojis = ['Thumbs Up Sign', 'Thumbs Down Sign']
+
+
+@bot.command(name = 'rpt', help = "Repeats text entered after the command.  Put sentences in quotes.")
+async def rpt(ctx, arg):
+    await ctx.send(arg)
+@bot.event
+async def on_reaction_add(reaction, user):
+  message = await bot
+  for emoji in Emojis:
+    await message.add_reaction(emoji)
+@bot.command(name='quote', help = 'will eventualy repeat the user\'s text')
 async def u_repeat(ctx):
   quote = [
     'apple',
     '_',
     (
-      'AH'
+      "AAAAAAAAAAAAAAAAAAAAA"
     )
   ]
   response = random.choice(quote)
--- transfer.py
+++ transfer.py
@@ -1,23 +1,15 @@
-import os
 import discord
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.environ['Discord_TOKEN']
-GUILD = os.environ['Discord_Guild']
+import pandas as pd
 
 client = discord.Client()
+guild = discord.Guild
 
-@client.event 
-async def on_ready():
-  guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
-    # When the bot is ready
-  print(
-    f"{client.user} has connected to the server:\n" 
-    f'{guild.name}(id:{guild.id})'
-  )
-  
-client.run(TOKEN)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    elif message.content.startswith('_'):
 
-
+        cmd = message.content.split()[0].replace("_","")
+        if len(message.content.split()) > 1:
+            parameters = message.content.split()[1:]
